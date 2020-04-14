35406dd4cf980d52e1c1a729d5cca69ad3813f7b
ccx1010sky <35686804+ccx1010sky@users.noreply.github.com>
2020-04-14 22:38:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const app = new express();
 app.get("/", function (req, res) {
   res.send("hi index ");
 });
-app.listen(3000, "localhost");
+app.listen(3000, (err) => {
+    console.log('app started');
+  });
