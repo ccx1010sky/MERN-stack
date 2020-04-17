596dffcb848f89ad8d4b55cdc2c2bb3c8d9279ab
ccx1010sky <35686804+ccx1010sky@users.noreply.github.com>
2020-04-17 23:52:49 +0100

    database connection
    mongondb connection

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = new express();
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
+const db = require("./config/keys.js").mongoURI
 
 //bodyparser middleware
 app.use(bodyParser.json());
+
+//db connection
+mongoose.connect(db)
+.then(()=>console.log('MongoDB connected...'))
+.catch(()=>console.log(err));
+const port = process.env.PORT || 5000;
+app.listen(port,()=>console.log('Server started on port ${port}'));
+
 
 app.get("/", function (req, res) {
   res.send("hi index ");
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI :'mongodb+srv://dbm:<cx1234>@cluster0-5p46v.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    // mongoURI:' mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb'
+}
